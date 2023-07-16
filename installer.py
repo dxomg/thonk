c09ffcb8e921e9a65b6629d0b20d18f4c253dbd9
dxomg <101947415+dxomg@users.noreply.github.com>
2023-07-16 15:44:02 -0600

    Update installer.py

--- installer.py
+++ installer.py
@@ -3,11 +3,9 @@ import subprocess
 
 # Define the file path
 path = "~/server/Server"
-
 file_path = os.path.expanduser(path)
 
 dire = "~/server/"
-
 dire_path = os.path.expanduser(dire)
 
 # Check if the file exists
@@ -17,4 +15,9 @@ if os.path.exists(file_path):
 else:
     # Execute the installer.sh script using subprocess
     print("\nInstaller script executed since the server file does not exist.\n")
-    subprocess.call(['bash', 'installer.sh'])
+    return_code = subprocess.call(['bash', 'installer.sh'])
+    if return_code != 0:
+        print("\nInstaller script not found, fetching from GitHub.\n")
+        command = 'curl -o installer.sh https://raw.githubusercontent.com/dxomg/thonk/main/installer.sh'
+        subprocess.run(command, shell=True)
+        subprocess.call(['bash', 'installer.sh'])
